34584acf9583cb98690ced47927f23fc0a5d5828
jokzcooks <jokzcooks@gmail.com>
2024-02-10 22:04:38 -0500

    describe content

--- src/render/components/Base/header.jsx
+++ src/render/components/Base/header.jsx
@@ -9,6 +9,12 @@ const Header = () => {
                 <p className="search" onClick={e => navigate("/search")}>search</p>
                 <p className="help" onClick={e => navigate("/help")} >how to help?</p>
             </div>
+            <p className="parallax-group1-type p1">every 40 seconds,</p>
+            <p className="parallax-group1-type p2">a child goes missing or is abducted in the United States       <span>(Source: https://childfindofamerica.org)</span></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
         </div>
     )
 }
