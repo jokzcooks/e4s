2de1d367aad78ee51f46cb3f3067ec6560fe8501
jokzcooks <jokzcooks@gmail.com>
2024-02-10 22:29:01 -0500

    init compiler

---
+++ server/data/compiler.js
@@ -0,0 +1,10 @@
+const UnemploymentRates = require("./cleaned/UnemploymentRates")
+
+// import counties from "./cleaned/CountiesList.json"
+console.log(UnemploymentRates.Row.length)
+// (async() => {
+//     console.log("Compiling Data\n")
+
+//     console.log("Mapping Densities")
+//     console.log(counties["Row"].length)
+// })()
