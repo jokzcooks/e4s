45ee7f2970c557f8a45293298603bfd303ad33a3
jokzcooks <jokzcooks@gmail.com>
2024-02-11 05:41:52 -0500

    currr

--- src/render/components/Images/index.jsx
+++ src/render/components/Images/index.jsx
@@ -2,7 +2,11 @@ export {
     default as MissingChildren
   }
   from './images/missingChildren.png';
-export {
-    default as Chevron
-}
-from './images/chevron.svg';
+  export {
+      default as Chevron
+  }
+  from './images/chevron.svg';
+  export {
+      default as ChevronFlipped
+  }
+  from './images/chevronFlipped.svg';
